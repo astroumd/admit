038cac77f68b61adce3e941b3988b9c83c924aea
Peter Teuben <teuben@gmail.com>
2016-09-28 14:11:46 -0400

    narrow the commands for better printout and understanding

--- scripts/casa.init.py
+++ scripts/casa.init.py
@@ -4,11 +4,12 @@
 #
 import os
 import sys
-    
+from os.path import join
 
 try:
     admit_path = os.environ['ADMIT']
     sys.path.append(admit_path)
-    os.environ["PATH"] += os.pathsep + admit_path + '/bin/'  + os.pathsep + '/usr/local/bin/'
+    os.environ["PATH"] += os.pathsep + join(admit_path,'bin')
+    os.environ["PATH"] += os.pathsep + '/usr/local/bin/'
 except KeyError:
     print("ADMIT path not defined. If you wish to use ADMIT, source the admit_start.[c]sh file.")

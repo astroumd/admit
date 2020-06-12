2e92b1b0dd539b6053fa15192dc9076417d98cb5
Peter Teuben <teuben@gmail.com>
2020-06-12 08:13:08 -0400

    fix writing new style ascii spectra when only one input bdp

--- admit/at/CubeSpectrum_AT.py
+++ admit/at/CubeSpectrum_AT.py
@@ -141,7 +141,7 @@ class CubeSpectrum_AT(AT):
                 "xaxis"   : "",    # currently still ignored
         }
         AT.__init__(self,keys,keyval)
-        self._version       = "1.2.3"
+        self._version       = "1.2.4"
         self.set_bdp_in( [(Image_BDP,       1,bt.REQUIRED),     # 0: cube: SpwCube or LineCube allowed
                           (CubeStats_BDP,   1,bt.OPTIONAL),     # 1: stats, uses maxpos
                           (Moment_BDP,      1,bt.OPTIONAL),     # 2: map, uses the max in this image as pos=
@@ -389,7 +389,11 @@ class CubeSpectrum_AT(AT):
 
         if True:
             #       @todo     only first plane due to limitation in exportTable()
-            nspec = table.data.shape[2]
+            ndim =  len(table.data.shape)
+            if ndim > 2:
+                nspec = table.data.shape[2]
+            else:
+                nspec = 1
             logging.info("Writing %d testCubeSpectrum tables" % nspec)
             for ispec in range(nspec):
                 islash = bdp_name.find('/')

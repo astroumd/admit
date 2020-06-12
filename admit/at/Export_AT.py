a2c755de53abba304495844a1f2b4f3b4a9fb8ba
Peter Teuben <teuben@gmail.com>
2020-06-11 20:27:12 -0400

    converted to python 3 - allow writing of tables

--- admit/at/Export_AT.py
+++ admit/at/Export_AT.py
@@ -5,32 +5,39 @@
 
    This module defines the Export_AT class.
 """
+import types
+import os
+import numpy as np
+import numpy.ma as ma
+from copy import deepcopy
+
 from admit.AT import AT
 from admit.Summary import SummaryEntry
 import admit.util.bdp_types as bt
 import admit.util.casautil as casautil
+from admit.util import utils, specutil
 import admit.util.Image as Image
 import admit.util.Line as Line
 import admit.util.ImPlot as ImPlot
 from admit.bdp.Image_BDP import Image_BDP
+from admit.bdp.Table_BDP import Table_BDP
+from admit.bdp.CubeSpectrum_BDP import CubeSpectrum_BDP
 from admit.bdp.CubeStats_BDP import CubeStats_BDP
 from admit.bdp.LineList_BDP import LineList_BDP
 from admit.bdp.Moment_BDP import Moment_BDP
 import admit.util.utils as utils
 import admit.util.filter.Filter1D as Filter1D
 from admit.util.AdmitLogging import AdmitLogging as logging
-import numpy as np
-import numpy.ma as ma
-from copy import deepcopy
 
-import types
-import os
 try:
-  import casa
-  import taskinit
+    import casa
 except:
-  print("WARNING: No CASA; Export task cannot function.")
+    try:
+        import casatasks as casa
+    except:
+        print("WARNING: No CASA; Export task cannot function.")
 
+  
 class Export_AT(AT):
     """Creates a FITS file from an image BDP.
 
@@ -40,7 +47,7 @@ class Export_AT(AT):
 
     At a future date this task will also export other types of BDPs, e.g. tables
 
-    Note that the output file is always overwritten.
+    WARNING: The output file is always silently overwritten.
 
     **Keywords**
 
@@ -83,8 +90,9 @@ class Export_AT(AT):
             "basename"   : "",     # defaults to BDP derived
         }
         AT.__init__(self,keys,keyval)
-        self._version = "1.0.0"
-        self.set_bdp_in([(Image_BDP,     1, bt.REQUIRED)])
+        self._version = "1.2.0"
+        self.set_bdp_in([(Image_BDP,    1, bt.OPTIONAL),
+                         (Table_BDP,    1, bt.OPTIONAL)])
         self.set_bdp_out([])
 
     def run(self):
@@ -106,26 +114,55 @@ class Export_AT(AT):
         logging.info("Found %d input BDPs" % nbdp)
         if nbdp > 1:
             logging.info("Only dealing with 1 BDP now")
-        
- 
-        b1  = self._bdp_in[0]                      # image/cube
-        infile = b1.getimagefile(bt.CASA)          # ADMIT filename of the image (cube)
-
-        if len(basename) == 0:
-            fitsname = self.mkext(infile,'fits')   # morph to the new output name with replaced extension '
-            image_out = self.dir(fitsname)         # absolute filename
-        else:
-            if basename[0:2] == './' or basename[0] == '/':
-                image_out = basename + ".fits"
-            else:
-                image_out = self.dir(basename + ".fits")
-        
+          
         dt.tag("start")
 
-        logging.info("Writing FITS %s" % image_out)
+        if self._bdp_in[0]._type == bt.CUBESPECTRUM_BDP:
+            specs = specutil.getspectrum(self._bdp_in[0])
+            nspec = len(specs)
+            nchan = len(specs[0])
+            spectra = list(range(nspec))
+            freqs  = specs[0].freq(0)
+            for i in range(nspec):
+                spectra[i] = specs[i].spec(0)
 
-        # @todo   check self.dir(image_out)
-        casa.exportfits(self.dir(infile), image_out, overwrite=True)
+            if len(basename) == 0:
+                tabname = self.mkext(infile,'tab')     # morph to the new output name with replaced extension '
+                table_out = self.dir(tabname)          # absolute filename
+            else:
+                if basename[0:2] == './' or basename[0] == '/':
+                    table_out = basename + ".tab"
+                else:
+                    table_out = self.dir(basename + ".tab")
+                    
+            logging.info("Writing %s" % table_out)
+            fp = open(table_out,"w")
+            fp.write('#  freq flux ...\n')
+            for j in range(nchan):
+                line = str(freqs[j])
+                for i in range(nspec):
+                    line = line + ' ' + str(spectra[i][j])
+                fp.write(line + '\n')
+            fp.close()
+                
+                  
+        if self._bdp_in[0]._type == bt.MOMENT_BDP:
+            b1  = self._bdp_in[0]                      # image/cube
+            infile = b1.getimagefile(bt.CASA)          # ADMIT filename of the image (cube)
+
+            if len(basename) == 0:
+                fitsname = self.mkext(infile,'fits')   # morph to the new output name with replaced extension '
+                image_out = self.dir(fitsname)         # absolute filename
+            else:
+                if basename[0:2] == './' or basename[0] == '/':
+                    image_out = basename + ".fits"
+                else:
+                    image_out = self.dir(basename + ".fits")
+        
+            logging.info("Writing FITS %s" % image_out)
+
+            # @todo   check self.dir(image_out)
+            casa.exportfits(self.dir(infile), image_out, overwrite=True)
         
         dt.tag("done")
         dt.end()

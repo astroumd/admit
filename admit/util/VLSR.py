8bbd815f9b2f738007978ea6d4f6d919b966992b
Peter Teuben <teuben@gmail.com>
2020-10-16 19:21:35 -0400

    add vlsrz() method from reading z.tab

--- admit/util/VLSR.py
+++ admit/util/VLSR.py
@@ -39,6 +39,7 @@ class VLSR(object):
 
     - merge in multiple tables, now hardcoded to a single
       table in $ADMIT/etc/vlsr.tab
+    - added a vlsrz() method using z.tab (oct-2020)
     - allow astroquery from NED and/or SIMBAD
       (this may need to run in pure-python, outside of ADMIT/CASA)
     - name matching is now done in upper case by default
@@ -49,21 +50,27 @@ class VLSR(object):
     """
 
     def __init__(self, upper=True):
-        self.version = "27-apr-2016"
+        self.version = "16-oct-2020"
         if have_ADMIT:
-            self.table = utils.admit_root() + "/etc/vlsr.tab"
-            self.cat = read_vlsr(self.table,upper)
-            logging.debug("VLSR: %s, found %d entries" % (self.table,len(self.cat)))
+            self.table1 = utils.admit_root() + "/etc/vlsr.tab"
+            self.table2 = utils.admit_root() + "/etc/z.tab"            
+            self.cat  = read_vlsr(self.table1,upper)
+            self.zcat = read_z(self.table2,upper)
+            #    while we're tinkering with VLSR determination, make this "info"
+            #logging.debug("VLSR: %s, found %d entries" % (self.table1,len(self.cat)))
+            #logging.debug("VLSR: %s, found %d entries" % (self.table2,len(self.zcat)))
+            logging.info("VLSR: %s, found %d entries" % (self.table1,len(self.cat)))
+            logging.info("VLSR: %s, found %d entries" % (self.table2,len(self.zcat)))
         else:
-            logging.warning("VLSR: Warning, no ADMIT, empty catalogue")
+            logging.warning("VLSR: Warning, no ADMIT, empty VLSR/Z catalogue")
             self.cat = {}
+            self.zcat = {}                               
 
     def vlsr(self, name, upper=True):
         """ return VLSR from requested object
             name matching is done in upper case by default
             If no match is found, 0.0 is returned.
         """
-        # print "VLSR debug",name,self.cat
         if len(name) == 0:  return 0.0
         src = name.upper()
         quote1 = "'"
@@ -76,6 +83,24 @@ class VLSR(object):
             return self.cat[src]
         else:
             return 0.0
+
+    def vlsrz(self, name, c=299792.458, upper=True):
+        """ return VLSR from Z table for a requested object
+            name matching is done in upper case by default
+            If no match is found, 0.0 is returned.
+
+            NOTE: normally c * z/(1+z) is returned, unless c<0
+                  it will return z itself.
+        """
+        if len(name) == 0:  return 0.0
+        src = name.upper()
+        if src in self.zcat:
+            if c < 0:
+                return self.zcat[src]
+            else:
+                return c*self.zcat[src]/(1+self.zcat[src]) 
+        else:
+            return np.array([0.0])
 
     def vlsr2(self, name):
         """ experimental Simbad/NED
@@ -109,7 +134,7 @@ class VLSR(object):
         return 0.0
 
 def read_vlsr(filename, upper=True):
-    """ read the ADMIT vlsr.cat table
+    """ read the ADMIT vlsr.tab table
 
     This table is peculiar in allowing spaces in source names,
     but then requires the name to be single (or double) quotes.
@@ -155,6 +180,39 @@ def read_vlsr(filename, upper=True):
             if upper:
                 src = src.upper()
             cat[src] = float(w[1])
+    return cat
+
+def read_z(filename, upper=True):
+    """ read the ADMIT z.tab table
+
+    This table is peculiar in allowing spaces in source names,
+    but then requires the name to be single (or double) quotes.
+    The source name is stored without these quotes.
+    The 2nd column is VLSR in km/s
+    Anything beyond is ignored, so comments are allowed 
+    Also note we are taking the source names "as is", so 
+    it is suggested they are in upper case.
+    
+    """
+    with open(filename, mode='r') as fp:
+        lines = fp.readlines()
+        cat = {}
+        ns = 0
+        for line in lines:
+            if line[0] == '#': continue
+            w = line.strip().split(',')
+            if len(w) > 1:
+                #print(w[0],w[1])
+                ns = ns + 1
+                if w[0] in cat:
+                    cat[w[0]].append(float(w[1]))
+                else:
+                    cat[w[0]] = [float(w[1])]
+
+    for s in cat.keys():
+        cat[s] = np.array(cat[s])
+        # print(s,cat[s].mean(),cat[s].std())
+                
     return cat
 
 if __name__ == "__main__":
